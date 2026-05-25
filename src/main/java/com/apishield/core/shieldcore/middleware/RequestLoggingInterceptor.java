aa93bbb4de38f9fd962ac0ed344eaf1646c9e1ff
Gustavo <narugu@hotmail.com>
2026-05-25 12:29:23 -0300

    feat: add request execution time logging

--- src/main/java/com/apishield/core/shieldcore/middleware/RequestLoggingInterceptor.java
+++ src/main/java/com/apishield/core/shieldcore/middleware/RequestLoggingInterceptor.java
@@ -8,12 +8,18 @@ import org.springframework.web.servlet.HandlerInterceptor;
 @Component
 public class RequestLoggingInterceptor implements HandlerInterceptor {
 
+    private static final String START_TIME = "startTime";
+
     @Override
     public boolean preHandle(
             HttpServletRequest request,
             HttpServletResponse response,
             Object handler
     ) throws Exception {
+
+        long startTime = System.currentTimeMillis();
+
+        request.setAttribute(START_TIME, startTime);
 
         String method = request.getMethod();
         String endpoint = request.getRequestURI();
@@ -29,5 +35,29 @@ public class RequestLoggingInterceptor implements HandlerInterceptor {
         );
 
         return true;
+    }
+
+    @Override
+    public void afterCompletion(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            Object handler,
+            Exception ex
+    ) throws Exception {
+
+        long startTime = (long) request.getAttribute(START_TIME);
+
+        long executionTime = System.currentTimeMillis() - startTime;
+
+        int status = response.getStatus();
+
+        System.out.println(
+                "[API SHIELD] Response completed -> " +
+                        "Status: " +
+                        status +
+                        " | Execution Time: " +
+                        executionTime +
+                        "ms"
+        );
     }
 }

9254105a350d49ec99596d385f59fc21cbe7072c
Gustavo <narugu@hotmail.com>
2026-06-02 19:44:28 -0300

    dia 8 part 1

---
+++ src/main/java/com/apishield/core/shieldcore/domain/RateLimitInfo.java
@@ -0,0 +1,29 @@
+package com.apishield.core.shieldcore.domain;
+
+public class RateLimitInfo {
+
+    private int requestCount;
+
+    private long windowStartTime;
+
+    public RateLimitInfo(int requestCount, long windowStartTime) {
+        this.requestCount = requestCount;
+        this.windowStartTime = windowStartTime;
+    }
+
+    public int getRequestCount() {
+        return requestCount;
+    }
+
+    public void setRequestCount(int requestCount) {
+        this.requestCount = requestCount;
+    }
+
+    public long getWindowStartTime() {
+        return windowStartTime;
+    }
+
+    public void setWindowStartTime(long windowStartTime) {
+        this.windowStartTime = windowStartTime;
+    }
+}
